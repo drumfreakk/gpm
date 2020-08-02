6002d074a131afe95a5f7259a8487e911cf8c77d
Kieran H <kieran.houtgraaf@gmail.com>
2020-08-02 15:34:42 +0200

    Caught curl message data

--- main.c
+++ main.c
@@ -1,20 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <curl/curl.h>
 #include "config.h"
 
 size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp);
 
+struct string{
+	char *string;
+	size_t size;
+};
+
 int main(void){
 	CURL *curl;
 	CURLcode res;
  
+	struct string output;
+	output.string = malloc(1);
+	output.size = 0;
+
 	curl_global_init(CURL_GLOBAL_DEFAULT);
- 
 	curl = curl_easy_init();
+	
 	if(curl) {
 		curl_easy_setopt(curl, CURLOPT_URL, "https://api.github.com/repos/kippenjongen/gpm-test-repo/releases");
 		curl_easy_setopt(curl, CURLOPT_USERAGENT, USER_AGENT);
-//		curl_easy_setopt(easyhandle, CURLOPT_WRITEFUNCTION, write_data); 
+		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, &output);
 
 		res = curl_easy_perform(curl);
 		if(res != CURLE_OK){
@@ -23,10 +35,30 @@ int main(void){
 		curl_easy_cleanup(curl);
 	}
 	curl_global_cleanup();
+
+	for(int i = 0; i < output.size; i++){
+		printf("%c", output.string[i]);
+	}
+
+	free(output.string);
 	return 0;
 }
-
 
 size_t write_data(void *buffer, size_t size, size_t nmemb, void *userp){
-	return 0;
+	struct string *output = (struct string *) userp;
+
+	char *ptr = realloc(output->string, output->size + nmemb + 1);
+	if(ptr  == NULL){
+		printf("well fuck\n");
+		return 0;
+	}
+
+	output->string = ptr;
+	memcpy(&(output->string[output->size]), buffer, nmemb);
+	output->size += nmemb;
+	output->string[output->size] = 0;
+
+	return nmemb;
 }
+
+
